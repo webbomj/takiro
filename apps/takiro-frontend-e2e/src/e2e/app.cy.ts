3aeb4edb99bd4f3a81088a22035d91312b738de4
webbomj <nethack@mail.ru>
2025-01-20 16:07:22 +0300

    fix: update e2e test

--- apps/takiro-frontend-e2e/src/e2e/app.cy.ts
+++ apps/takiro-frontend-e2e/src/e2e/app.cy.ts
@@ -7,7 +7,5 @@ describe('takiro-frontend-e2e', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Hello NX');
   });
 });
